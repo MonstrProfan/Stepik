361b4b34004693e154219347dc75ed65a41cdf77
Petr Mihkin <petrmihkin@mail.ru>
2019-05-22 09:23:06 +0300

    Вывод строк текста в консоль

---
+++ Вывод строки текста в консоль/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Вывод_строки_текста_в_консоль
+    {
+    class Program
+        {
+        static void Main ()
+            {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Hello, BeeGeek!");
+
+            Console.ReadKey();
+            }
+        }
+    }
