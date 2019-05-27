1edfcf816391e1d64796f4b7b3fc4d81004b164e
Petr Mihkin <petrmihkin@mail.ru>
2019-05-27 15:28:53 +0300

    Цена пирожков

---
+++ разложенная на руб. и коп. стоимость пирожков/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace разложенная_на_руб.и_коп.стоимость_пирожков
+    {
+    class Program
+        {
+        static void Main ()
+            {
+            /*
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
+            */
+
+            int a = 5;
+            int b = 50;
+            int n = 2;
+
+            a = a * n + (b * n / 100);
+            b = b * n % 100;
+
+            Console.WriteLine($"{a} {b}");
+
+            Console.ReadKey();
+            }
+        }
+    }
