b7c6b0fbf84c63d1e07966404b36c15a90ffc3e8
Petr Mihkin <petrmihkin@mail.ru>
2019-05-29 09:21:25 +0300

    среднее арифметическое целых чисел

---
+++ Среднее арифметическое целых чисел/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Среднее_арифметическое_целых_чисел
+    {
+    class Program
+        {
+        static void Main ()
+            {
+            /*среднее арифметическое целых чисел*/
+
+            /*
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            */
+            int a = 1;
+            int b = 3;
+
+            double c = (double)(a + b) / 2;
+
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+            }
+        }
+    }
