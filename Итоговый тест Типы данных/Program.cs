37f553a77eeea48ed5573bd1b70717fa822d18ca
Petr Mihkin <petrmihkin@mail.ru>
2019-05-30 10:33:18 +0300

    n-й член арифметической прогрессии

--- Итоговый тест Типы данных/Program.cs
+++ Итоговый тест Типы данных/Program.cs
@@ -7,7 +7,7 @@ namespace Итоговый_тест_Типы_данных
         static void Main()
         {
             /*гипотенуза треугольника с заданными катетами*/
-
+            #region
             /*
             double a = Convert.ToDouble(Console.ReadLine());
             double b = Convert.ToDouble(Console.ReadLine());
@@ -19,9 +19,9 @@ namespace Итоговый_тест_Типы_данных
             double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
 
             Console.WriteLine(c);
-
+            #endregion
             /*нахождение сотен числа*/
-
+            #region
             /*
              int a1 = Convert.ToInt32(Console.ReadLine());
             */
@@ -31,6 +31,17 @@ namespace Итоговый_тест_Типы_данных
             a1 = (a1 / 100)%10;
             
             Console.WriteLine(a1);
+            #endregion
+            /*n-й член арифметической прогрессии*/
+            #region
+            int aa = Convert.ToInt32(Console.ReadLine());
+            double d = Convert.ToDouble(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            aa = aa + (int)(Math.Round(d) * (n - 1));
+
+            Console.WriteLine($"{aa}");
+            #endregion
 
             Console.ReadKey();
         }
