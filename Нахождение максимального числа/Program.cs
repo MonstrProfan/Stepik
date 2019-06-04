9cc12efbb972ea41a07eae2ecba09d44e9b09591
Petr Mihkin <petrmihkin@mail.ru>
2019-06-04 09:51:23 +0300

    зона боя слона

--- Нахождение максимального числа/Program.cs
+++ Нахождение максимального числа/Program.cs
@@ -140,6 +140,23 @@ namespace Нахождение_максимального_числа
                 Console.WriteLine("NO");
             }
 
+            /*зона слона*/
+
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            if ((x1 + y1 == x2 + y2) || (x1 - y1 == y2 - y2))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+
+            }
+
 
             Console.ReadKey();
         }
