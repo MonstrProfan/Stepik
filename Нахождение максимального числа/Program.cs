e711263d1958ed91d6c8cd109de88c326c549949
Petr Mihkin <petrmihkin@mail.ru>
2019-06-04 09:42:32 +0300

    определение счастливого номера билета

--- Нахождение максимального числа/Program.cs
+++ Нахождение максимального числа/Program.cs
@@ -11,7 +11,7 @@ namespace Нахождение_максимального_числа
               int a = Convert.ToInt32(Console.ReadLine());
               int b = Convert.ToInt32(Console.ReadLine());
             */
-            int a = 3;
+            int a = 313;
             int b = 3;
 
             if (a == b)
@@ -73,6 +73,57 @@ namespace Нахождение_максимального_числа
             }
 
             //Console.WriteLine(c);
+
+
+            /*определение различности цифр в разрядах числа*/
+
+            int a1 = a / 100;
+            int a2 = (a - a1 * 100)/10;
+            int a3 = a % 10;
+
+            if (a1 != a2 && a2 != a3 && a1 != a3)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+            //Console.WriteLine($"{a}\t {a1}\t {a2}\t {a3}");
+
+            /*определение счастливого билета*/
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            int b = (int)Math.Abs(x);
+            int x1 = b / 100000;
+
+            int x2 = (b - x1 * 100000) / 10000;
+            int x22 = b - x1 * 100000;
+
+            int x3 = x2 * 10000;
+            int x33 = x22 - x3;
+            x3 = x33 / 1000;
+
+            int x4 = x3 * 1000;
+            int x44 = x33 - x4;
+            x4 = x44 / 100;
+
+            int x5 = x4 * 100;
+            int x55 = x44 - x5;
+            x5 = x55 / 10;
+
+            int x6 = b % 10;
+
+            if (x1 + x2 + x3 == x4 + x5 + x6)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
 
             Console.ReadKey();
         }
